c9fbbfc9f9c401fadb2956e70a36e264ccde33b4
EigenWelt <84897077+EigenWelt@users.noreply.github.com>
2021-05-28 11:45:32 +0800

    Update register.py

--- register.py
+++ register.py
@@ -1,33 +1,34 @@
-# encoding:utf-8
-import requests
-import base64
 from aip import AipFace
-import cv2
-import json
-client_id = 'jO83mibjqItqUZfijp6MnpYF'
-client_secret = 'xNFDBFafiFuRXPMAObwMv1rOcq38Yh8s'
-APP_ID='24250074'
-# client_id 为官网获取的AK， client_secret 为官网获取的SK
-host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id='+client_id+'&client_secret='+client_secret
-response = requests.get(host)
-if response:
-    t = response.json()
-    access_token = t['access_token']
+import base64
 
-client = AipFace(APP_ID, client_id, client_secret)
-cap = cv2.VideoCapture(0)
-ret, frame = cap.read()
-image = base64.b64encode(frame).decode()
-face_add = 'https://aip.baidubce.com/rest/2.0/face/v3/faceset/user/add?access_token='+access_token
-headers = {
-    'Content-Type': 'application/json'
-}
-data = {
-    'image': image,
-    'image_type': 'BASE64',
-    'group_id': 'archive',
-    'user_id': 'zhang',
-}
-res = requests.post(url=face_add, headers=headers, data=data)
-res = json.loads(res.text)
-print(res)
+""" 你的 APPID AK SK """
+from aip import AipFace
+
+""" 你的 APPID AK SK """
+APP_ID = '24250074'
+API_KEY = 'jO83mibjqItqUZfijp6MnpYF'
+SECRET_KEY = 'xNFDBFafiFuRXPMAObwMv1rOcq38Yh8s'
+
+client = AipFace(APP_ID, API_KEY, SECRET_KEY)
+
+
+f = open('test.jpg', 'rb')
+pic = base64.b64encode(f.read())
+image = str(pic, 'utf-8')
+
+imageType = "BASE64"
+
+groupId = "group1"
+
+userId = "user1"
+
+""" 如果有可选参数 """
+options = {}
+options["user_info"] = "123"
+options["quality_control"] = "NORMAL"
+options["liveness_control"] = "LOW"
+options["action_type"] = "REPLACE"
+
+""" 带参数调用人脸注册 """
+result = client.addUser(image, imageType, groupId, userId, options)
+print(result)
